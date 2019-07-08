3d7a2ee99f6dca22621e9b812c6f71885fc79c42
DanyAdhiPrabowo <50516766+DanyAdhiPrabowo@users.noreply.github.com>
2019-07-08 21:14:46 +0700

    Update connect.js

--- connect.js
+++ connect.js
@@ -4,10 +4,10 @@ const mysql = require('mysql');
 
 const con 	= mysql.createConnection ({
 	
-	host	: process.env.DB_HOST,
-	user	: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_DATABASE
+	host	: 'https://remotemysql.com',
+	user	: kRvQIndd7l,
+	password: rQ1RJ0R5lY,
+	database: kRvQIndd7l
 })
 
 con.connect(function(error){
